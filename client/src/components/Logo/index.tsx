6400e917b603e38606a04b5fb5fcce243daba5af
manc1n1 <mancinij1111@gmail.com>
2026-05-27 18:57:53 -0400

    refactor: improve responsive header branding

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -1,14 +1,9 @@
 import styles from './Logo.module.css';
 
-import { useLocation } from 'react-router-dom';
-
 import Flippers from '@/assets/images/flippers.png';
 import DarkFlippers from '@/assets/images/dark_flippers.png';
 
 const Logo = () => {
-  const { pathname } = useLocation();
-  const showWebsiteName = pathname === '/';
-
   return (
     <div className={styles.logoContainer}>
       <picture className={styles.logo}>
@@ -25,7 +20,7 @@ const Logo = () => {
           alt='Logo'
         />
       </picture>
-      {showWebsiteName && <div className={styles.name}>flipp.rs</div>}
+      <div className={styles.name}>flipp.rs</div>
     </div>
   );
 };
